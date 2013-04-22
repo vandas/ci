57aad804d29f2c32e2d2b689bc9ec9fefdc0d44a
vandas <sjrlxs@gmail.com>
2013-04-22 17:33:32 +0800

    V1.0

---
+++ application/controllers/hello.php
@@ -0,0 +1,15 @@
+<?php 
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Hello extends CI_Controller 
+{
+	function sayhello() {
+		echo 'hello word';
+	}
+	
+
+	
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
